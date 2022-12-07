8b9810fe8c31585b8e5b0c3dd75b8f1cedad4bca
zheniaStukalo <82517499+t0xick1d@users.noreply.github.com>
2022-12-07 15:22:43 +0200

    complite task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
+
+const pictureConteiner = document.querySelector('.gallery');
+
+const cardMarkup = createPicturesCardMarkup(galleryItems);
+
+pictureConteiner.addEventListener('click', onPictureClick);
+
+pictureConteiner.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createPicturesCardMarkup(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return ` 
+      <div class="gallery__item">
+        <a class="gallery__link" href=${original}>
+          <img
+            class="gallery__image"
+            src=${preview}
+            data-source=${original}
+            alt=${description}
+          />
+        </a>
+      </div>`;
+    })
+    .join('');
+}
+
+function onPictureClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const actualClickPictures = e.target.dataset.source;
+  const instance = basicLightbox.create(
+    `
+    <img src=${actualClickPictures} width="800" height="600">
+    `,
+    {
+      onShow: (instance) => {
+        window.addEventListener('keydown', onClosePictures);
+      },
+      onClose: (instance) => {
+        window.removeEventListener('keydown', onClosePictures);
+      },
+    },
+  );
+  instance.show();
+
+  function onClosePictures(e) {
+    e.code === 'Escape' && instance.close();
+  }
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const pictureConteiner = document.querySelector('.gallery');
+
+const cardMarkup = createPicturesCardMarkup(galleryItems);
+
+// pictureConteiner.addEventListener('click', onClickImg);
+
+pictureConteiner.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createPicturesCardMarkup(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+      </a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// function onClickImg(evt) {
+//   evt.preventDefault();
+
+//   const lightbox = new SimpleLightbox('.gallery a', {
+//     sourceArrr: evt.target.src,
+//   });
+
+//   lightbox.open(evt.target.src);
+
+//   console.log(evt.target.src);
+// }
+
 console.log(galleryItems);
